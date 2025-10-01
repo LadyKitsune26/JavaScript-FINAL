c1b91301da4a035be880e969722b7f911ec0cf87
LadyKitsune26 <langlois.raimey@gmail.com>
2025-10-01 18:46:17 -0500

    javascript at undefined

--- JSFINAL.js
+++ JSFINAL.js
@@ -2,4 +2,14 @@ function searchChange(){
     console.log(event.target.value);
 }
 
-fetch()
+async function renderMovies(){
+const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=3dd6eeee`)
+const data = await response.json();
+console.log(data.Search);
+}
+
+renderMovies()
+
+
+
+// fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=3dd6eeee`)
