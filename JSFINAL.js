428cd3248a0a8975cfdc83619526e0b6c307f482
LadyKitsune26 <langlois.raimey@gmail.com>
2025-10-01 19:26:27 -0500

    search input acting the same as the search title

--- JSFINAL.js
+++ JSFINAL.js
@@ -1,11 +1,16 @@
-function searchChange(){
+const moviesWrapper = document.querySelector
+
+
+function searchChange(event){
     console.log(event.target.value);
 }
 
 async function renderMovies(){
-const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=3dd6eeee`)
+const response = await fetch(`http://www.omdbapi.com/?s=fast&apikey=3dd6eeee`)
 const data = await response.json();
-console.log(data.Search);
+const moviesArr = data.Search;
+
+
 }
 
 renderMovies()
